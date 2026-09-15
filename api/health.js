ee83f6cf3a973057cb9d5c926b3400eadcca80d6
Rishikeshsanin <141367936+Rishikeshsanin@users.noreply.github.com>
2026-09-16 04:19:54 +0530

    Add Deloitte Interview health endpoint

---
+++ api/health.js
@@ -0,0 +1,1 @@
+module.exports=(req,res)=>{res.statusCode=200;res.setHeader('Content-Type','application/json');res.setHeader('Cache-Control','no-store');res.end(JSON.stringify({ok:true,service:'Deloitte Interview',aiConfigured:!!process.env.GEMINI_API_KEY}))};
